4df2301b536f7ee81de86d2cf3c38dbf3e6b993e
Jim Jones <jim.jones@uni-muenster.de>
2017-09-21 19:47:53 +0200

    Servlet implemented

--- wmss/src/de/wwu/wmss/web/ServletWMSS.java
+++ wmss/src/de/wwu/wmss/web/ServletWMSS.java
@@ -8,47 +8,79 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.http.HttpStatus;
-import org.eclipse.jetty.servlet.ServletContextHandler;
+public class ServletWMSS extends HttpServlet
+{
 
-public class ServletWMSS extends HttpServlet {
-
-
-	public ServletWMSS() {
-
-		super();
-
-	}
-
-	private static final long serialVersionUID = 5582283184868743560L;
-
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		Enumeration<String> listParameters = request.getParameterNames();
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{    	
+    	Enumeration<String> listParameters = request.getParameterNames();
+    	
+    	String format = "";
+    	String requestType = "";
+    	String group = "";
+    	String person = "";
+    	String personRole = "";
+    	String performanceMedium = "";
+    	String performanceMediumType = "";
+    	String solo = "";
+    	String tonalityTonic = "";
+    	String tonalityMode = "";
+    	String tempo = "";
+    	String creationDate = "";
+    	String creationDateFrom = "";
+    	String creationDateTo = "";
+    	String melody = "";
+    	
+    	System.out.println(request.getQueryString());
+    	
+    	while(listParameters.hasMoreElements() ) {
+    	       
+    		String parameter = (String) listParameters.nextElement();
+    		System.out.println(parameter + " -> "+ request.getParameter(parameter)+"");
+    		
+    		if (parameter.toLowerCase().equals("request")) {
+    			requestType=request.getParameter(parameter);    			
+    		} else if (parameter.toLowerCase().equals("format")) {
+    			format=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("group")) {
+    			group=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("person")) {
+    			person=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("personrole")) {
+    			personRole=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("performancemedium")) {
+    			performanceMedium=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("performancemediumtype")) {
+    			performanceMediumType=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("solo")) {
+    			solo=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("tonalitytonic")) {
+    			tonalityTonic=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("tonalitymode")) {
+    			tonalityMode=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("tempo")) {
+    			tempo=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("creationdate")) {
+    			creationDate=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("creationdatefrom")) {
+    			creationDateFrom=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("creationdateto")) {
+    			creationDateTo=request.getParameter(parameter); 
+    		} else if (parameter.toLowerCase().equals("melody")) {
+    			melody=request.getParameter(parameter); 
+    		} 
+    	       
+    	}
+    	
+    	
 		response.addHeader("Access-Control-Allow-Origin","*");
 		response.addHeader("Access-Control-Allow-Methods","GET,POST");
 		response.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-
-		response.setContentType("text/plain");
-		response.setCharacterEncoding("UTF-8");		
-		response.setStatus(HttpStatus.OK_200);
-
-		
-		response.getWriter().println("Web Music Score Service");
-
-		System.out.println("Incoming request ... " + request.getParameter("service"));
-		
-		while (listParameters.hasMoreElements()) {
-			
-			String parameter = (String) listParameters.nextElement();
-			System.out.println(parameter + " -> "+ request.getParameter(parameter)+"");
-
-		}
-	}
-
-
-
+    	
+    	response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>WMSS</h1>");
+        
+    }
 }
 
--- wmss/src/de/wwu/wmss/web/Start.java
+++ wmss/src/de/wwu/wmss/web/Start.java
@@ -1,9 +1,6 @@
 package de.wwu.wmss.web;
 
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -11,32 +8,17 @@ public class Start {
 
 	public static void main(String[] args) throws Exception {
 
-//		Server server = new Server(8282);
-//		ServletContextHandler handler = new ServletContextHandler(server, "/wmss");
-//		handler.addServlet(Servlet.class, "/");
-//		server.start();
-		
-
-		Server server = new Server(8282);
-		
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.setContextPath("/wmss");
-		
-    	server.setHandler(context);
-		
-		context.addServlet(new ServletHolder(new ServletWMSS()),"/*");
-		
-		
-		//ContextHandler contextGUI = new ContextHandler("/admin");
-	    //contextGUI.setHandler(new HandlerGUI("Bonjoir"));
-	        
-//		ContextHandlerCollection contexts = new ContextHandlerCollection();
-//        contexts.setHandlers(new Handler[] { context });
+        Server server = new Server(8295);
         
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/wmss");
         server.setHandler(context);
-
-		server.start();
-		server.join();
+ 
+        context.addServlet(new ServletHolder(new ServletWMSS()),"/*");
+ 
+        server.start();
+        server.join();
+        
 	}
 
 }
