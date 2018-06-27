c213876d32d4b70555b1681a2e14d80792b68618
Jim Jones <jim.jones@uni-muenster.de>
2018-06-27 10:05:17 +0200

    Disables neo4j query logging.

--- wmss/src/de/wwu/wmss/connectors/Neo4jConnector.java
+++ wmss/src/de/wwu/wmss/connectors/Neo4jConnector.java
@@ -3,6 +3,8 @@ package de.wwu.wmss.connectors;
 import de.wwu.wmss.core.DataSource;
 import de.wwu.wmss.settings.Util;
 import java.util.Date;
+import java.util.logging.LogManager;
+
 import org.apache.log4j.Logger;
 import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
@@ -14,9 +16,14 @@ import org.neo4j.driver.v1.StatementResult;
 public class Neo4jConnector {
 	private static Driver driver;
 	private static Logger logger = Logger.getLogger("Neo4j-Connector");
-	
+		
 	public static StatementResult executeQuery(String cypher, DataSource ds){
 
+		/**
+		 * Disables logging mechanism of neo4j
+		 */
+		LogManager.getLogManager().reset();
+		
 		Date start = new Date();
 		driver = GraphDatabase.driver(ds.getHost(),AuthTokens.basic(ds.getUser(),ds.getPassword()));
 
--- wmss/src/de/wwu/wmss/web/Start.java
+++ wmss/src/de/wwu/wmss/web/Start.java
@@ -3,29 +3,26 @@ package de.wwu.wmss.web;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
 import de.wwu.wmss.settings.SystemSettings;
 
 public class Start {
 
 	public static void main(String[] args) throws Exception {
-
+				
 		SystemSettings.loadSystemSettings();
 		SystemSettings.loadDataSources();
-		
-        Server server = new Server(SystemSettings.getPort());
-        
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/"+SystemSettings.getService());
-        server.setHandler(context);
- 
-        context.addServlet(new ServletHolder(new ServletWMSS()),"/*");
- 
-        server.start();
-        server.join();
-        
-        
-        
+
+		Server server = new Server(SystemSettings.getPort());
+
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/"+SystemSettings.getService());
+		server.setHandler(context);
+
+		context.addServlet(new ServletHolder(new ServletWMSS()),"/*");
+
+		server.start();
+		server.join();
+
 	}
 
 }
