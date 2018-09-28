3a2a3496b952644304e4a8fda93f2bf5cb372f2e
Jim Jones <jim.jones@uni-muenster.de>
2018-09-28 19:02:25 +0200

    Minor changes to the jetty servelet

--- wmss/src/de/wwu/wmss/web/Start.java
+++ wmss/src/de/wwu/wmss/web/Start.java
@@ -8,7 +8,9 @@ import de.wwu.wmss.settings.SystemSettings;
 public class Start {
 
 	public static void main(String[] args) throws Exception {
-				
+		
+		org.eclipse.jetty.util.log.Log.setLog(null);		
+		
 		SystemSettings.loadSystemSettings();
 		SystemSettings.loadDataSources();
 
