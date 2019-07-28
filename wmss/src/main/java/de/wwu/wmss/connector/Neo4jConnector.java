453cc841678912b87c539bda6502dcfe753418dd
Jim Jones <jimjonesbr@gmail.com>
2019-07-28 15:32:40 +0200

    UI changes for RDF Import

--- wmss/src/main/java/de/wwu/wmss/connector/Neo4jConnector.java
+++ wmss/src/main/java/de/wwu/wmss/connector/Neo4jConnector.java
@@ -4,12 +4,16 @@ import de.wwu.wmss.core.DataSource;
 import de.wwu.wmss.settings.SystemSettings;
 import de.wwu.wmss.settings.Util;
 import java.util.Date;
+import java.util.concurrent.CompletionStage;
 import java.util.logging.LogManager;
 import org.apache.log4j.Logger;
 import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.StatementResultCursor;
+import org.neo4j.driver.v1.Transaction;
 
 public class Neo4jConnector {
 		
@@ -42,17 +46,67 @@ public class Neo4jConnector {
 			}
 		}
 		
+		//StatementResult result;
+		
+//		Driver driver = GraphDatabase.driver(ds.getHost(),AuthTokens.basic(ds.getUser(),ds.getPassword()));
+//		Session session = driver.session();
+//		CompletionStage<StatementResultCursor> cursorStage = session.runAsync(cypher);
+//		
+//		cursorStage.thenCompose(StatementResultCursor::listAsync)
+//        .whenComplete((records, error) -> {
+//               
+//        	if (records != null) {
+//            	   System.out.println( records );
+//            	   
+//               }
+//               else {
+//            	   error.printStackTrace();
+//               }
+//               
+//               session.closeAsync();
+//        });
+		
+
+		
+//		Driver driver = GraphDatabase.driver(ds.getHost(),AuthTokens.basic(ds.getUser(),ds.getPassword()));
+//		Session session = driver.session();
+//		CompletionStage<StatementResultCursor> cursorStage = session.runAsync(cypher);
+//			
+//		StatementResult result = null;	
+//		cursorStage.thenCompose(StatementResultCursor::listAsync)
+//        .whenComplete((records, error) -> {
+//               
+//        	
+//        	if (records != null) {
+//            	   //System.out.println( records );
+//            	for (int i = 0; i < records.size(); i++) {
+//            		result.list().add(records.get(i));            		
+//    			}
+//            	   
+//               }
+//               else {
+//            	   error.printStackTrace();
+//               }
+//               
+//               session.closeAsync();
+//        });
+//
+		
+		Driver driver = GraphDatabase.driver(ds.getHost(),AuthTokens.basic(ds.getUser(),ds.getPassword()));
 		StatementResult result;
 
-		try ( Session session = ds.getNeo4jConnectionDriver().session() )
+		try(Transaction tx = driver.session().beginTransaction())
 		{
-			result = session.run(cypher);
-		}
+			result = tx.run(cypher);
+			tx.success();
+			tx.close();
+		}	
 		
 		logger.debug(cypher);
 		logger.debug("Cypher query time ["+ds.getHost()+"]: " + Util.timeElapsed(start, new Date()));
 
-		ds.getNeo4jConnectionDriver().session().close();
+		//ds.getNeo4jConnectionDriver().session().close();
+		driver.session().close();
 		
 		return result;
 	}
