4fe2f72246bfa03c0bf4d69fba94ccfe7642a55f
Jim Jones <jim.jones@uni-muenster.de>
2019-08-20 15:43:14 +0200

    Add missing unit test class

--- wmss/src/test/java/de/wwu/wmss/junit/TestSuiteWMSS.java
+++ wmss/src/test/java/de/wwu/wmss/junit/TestSuiteWMSS.java
@@ -6,12 +6,13 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	StartWMSS.class,
-	ImportScores.class,	
+	ImportMusicXML.class,
+	ImportRDFScores.class,		
 	ListScoresRequest.class,
 	ExceptionsTest.class,
 	DescribeServiceRequest.class,
 	GetScore.class,
-	ImportScoreFromURL.class,
+	ImportRDFScoreFromURL.class,
 	DeleteScoreRequest.class
 })
 
